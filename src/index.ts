b6ccfd58ec984ff9872cb4dbace1ef3d7856aa43
Pawel <pgogola10@gmail.com>
2022-05-31 20:47:18 +0200

    Add typing to html elements.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const list = document.querySelector<HTMLUListElement>(
+  '[data-attribute="list"]'
+);
+const form = document.querySelector<HTMLFormElement>('[data-attribute="form"]');
+const input = document.querySelector<HTMLInputElement>(
+  '[data-attribute="input"]'
+);
