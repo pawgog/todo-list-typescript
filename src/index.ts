3bbdf769b03ab5a8a147dbd6cad0580ed8e43893
Pawel <pgogola10@gmail.com>
2022-06-08 20:45:47 +0200

    Add function display todo list

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { v4 as uuidV4 } from 'uuid';
 
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
 const list = document.querySelector<HTMLUListElement>(
   '[data-attribute="list"]'
 );
@@ -13,10 +20,22 @@ form?.addEventListener('submit', (e) => {
 
   if (input?.value === '' || input?.value == null) return;
 
-  const task = {
+  const task: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date(),
   };
+
+  addListItem(task);
 });
+
+function addListItem(taskObject: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.append(checkbox, taskObject.title);
+  item.append(label);
+  list?.append(item);
+}
